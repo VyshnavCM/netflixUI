16847b6aa9527ef785fdbcad34e8b44bd35fee19
Vyshnav_Vijayan <vvijayan854@gmail.com>
2023-11-15 01:01:33 +0530

    Api started

---
+++ lib/domain/core/api_endpoints.dart
@@ -0,0 +1,6 @@
+import 'package:netflix/core/string.dart';
+import 'package:netflix/infrastructure/api_key.dart';
+
+class ApiEndpoints {
+  static const downloads = "$kBaseUrl/movie/550?api_key=$apiKey";
+}
